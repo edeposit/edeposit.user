f834058389e2eff02347d906fbcffbd8a825bbaa
Jan Stavěl <stavel.jan@gmail.com>
2013-10-21 11:39:22 +0200

    added autogenerating of ePeriodical

--- edeposit/user/handlers.py
+++ edeposit/user/handlers.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from zope.component import queryUtility
 from zope.container.interfaces import IObjectAddedEvent, IObjectRemovedEvent,\
     IContainerModifiedEvent
@@ -8,9 +9,11 @@ from edeposit.user import MessageFactory as _
 def added(context,event):
     """When an object is added, create folder for registration of ePublications
     """
-    tool = api.portal.get_tool('translation_service')
-    title = tool.translate(msgid=u"Registration of ePublications", 
-                           domain='edeposit.user',
-                           context=context, 
-                           target_language='cs')
-    context.invokeFactory('edeposit.content.epublicationfolder','epublications', title=title)
+    # tool = api.portal.get_tool('translation_service')
+    # title = tool.translate(msgid=u"Registration of ePublications", 
+    #                        domain='edeposit.user',
+    #                        context=context, 
+    #                        target_language='cs')
+    context.invokeFactory('edeposit.content.epublicationfolder','epublications', title=u"Ohlášení ePublikací")
+    context.invokeFactory('edeposit.content.eperiodicalfolder','eperiodicals', title=u"Ohlášování ePeriodik")
+    context.invokeFactory('edeposit.content.bookfolder','books', title=u"Ohlášení tištěných knih")
